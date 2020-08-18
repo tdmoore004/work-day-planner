677898371d8e53a10a5d5d6be9059d42ab4a1553
Tanner Moore <tdmoore004@gmail.com>
2020-08-17 21:42:47 -0600

    Added functionality to dynamically create schedule time blocks with hour.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,6 +6,21 @@ const workDayTime = ["9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", 
 function createSchedule () {
     for (i = 0; i < workDayTime.length; i++) {
         console.log(workDayTime[i]);
+
+        // Creating rows for hour time blocks.
+        let timeBlock = document.createElement("section");
+        timeBlock.classList.add("row");
+        
+        // Hour section.
+        let hourBlock = document.createElement("div");
+        hourBlock.classList.add("hour", "col-2", "pt-2");
+        hourBlock.style.textAlign = "right";
+        hourBlock.textContent = workDayTime[i];
+        timeBlock.appendChild(hourBlock);
+        console.log(hourBlock);
+
+        document.querySelector(".container").appendChild(timeBlock);
+        console.log(timeBlock);
     }
 }
 
